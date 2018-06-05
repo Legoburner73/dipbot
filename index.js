d9c11ecc84b2326305ec0768bb8a1e6e5c72080f
LegoDev <legoplaysmcmail@gmail.com>
2018-06-05 13:02:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ global.fs=require('fs');
 global.COLOR=require('chalk');
 global.settings=require('./settings.json');
 
-function send(msg) {
-  
-}
-
 global.C.on('ready', () => {
   console.log(global.COLOR.bgBlackBright.yellow.bold('[Dip] ')+global.COLOR.bgBlackBright.green.bold('Ready!'));
 });
@@ -17,12 +13,14 @@ global.C.on('message', M => {
     let args = M.content.split(' ').splice(1);
     if(fs.existsSync("./cmds/"+cmd+".js")) {
       let temp = require("./cmds/"+cmd+".js");
-      temp(send,);
+      temp(M, { 'send':M.channel.send, 'reply':M.reply, 'author':M.author, 'guild':M.guild, 'args':args, 'react':M.react, 'id':M.id, 'channel':M.channel, 'mentions':M.mentions });
     }else{
       //No command.
     }
   }
 });
-global.C.on('rateLimit', info => {
-  console.log(global.COLOR.yellow.bold('[WARNING] ')+global.COLOR.white('I\'m being ratelimited!');
-}
+global.C.on('error', e => {
+  console.error(global.COLOR.red.bold('[CRITICAL] ')+global.COLOR.red(e));
+  process.exit(1);
+});
+global.C.login(global.settings.token);
